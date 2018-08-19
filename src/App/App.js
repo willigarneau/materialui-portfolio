78d9b257bc776a9dcc2442c1f0be80b17276f591
William Garneau <willi_garn@hotmail.com>
2018-08-19 12:22:17 -0400

    Fixed lint

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { i18nextProvider } from 'react-i18next';
-import logo from '../logo.svg';
 import './App.css';
 
 function App() {
---
+++ src/main/navigation/Router.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch />
+      </div>
+    </BrowserRouter>
+  );
+}
+
