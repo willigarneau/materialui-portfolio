a451e69364beb5287655434735175d5ac05d975e
William Garneau <willi_garn@hotmail.com>
2018-08-28 13:27:55 -0400

    Fixed lint

--- src/main/navigation/Router.js
+++ src/main/navigation/Router.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Welcome from '../../profile/containers/Welcome.jsx';
 import ProfilePage from '../../profile/containers/ProfilePage.jsx';
 
 export default function AppRouter() {
